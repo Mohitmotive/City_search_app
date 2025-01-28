276af869c5b688bab63d5f52fd2c325f4b8fbd19
Mohit Kumar <mohit.kumar@gomotive.com>
2025-01-28 09:58:04 +0530

    Delete Pods directory

--- Pods/Target Support Files/Pods-CitySearch/Pods-CitySearch-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CitySearch : NSObject
-@end
-@implementation PodsDummy_Pods_CitySearch
-@end
--- Pods/Target Support Files/Pods-CitySearchTests/Pods-CitySearchTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CitySearchTests : NSObject
-@end
-@implementation PodsDummy_Pods_CitySearchTests
-@end
